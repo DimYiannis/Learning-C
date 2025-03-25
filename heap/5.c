616093ae46ee34c6b513604de81f475c7c63c426
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-03-25 20:53:25 +0200

    Update 5.c

--- heap/5.c
+++ heap/5.c
@@ -13,72 +13,105 @@ Elements in Stack-2 are: 70 60 50 40 20
 #include <stdlib.h>
 #define MAX_SIZE 100
 
-// create empty stack
-int *createstack()
+// global array for both stacks
+int stack[MAX_SIZE];
+
+// top pointers for two stack
+int top1 = -1;       // starts from left
+int top2 = MAX_SIZE; // starts from right
+
+// check stack 1 is full
+int isfull()
 {
-    int *stack = (int *)malloc(MAX_SIZE * sizeof(int));
-    return stack;
-}
-// check if stack is empty
-int isempty(int top)
-{
-    return (top == -1);
+    return (top1 + 1 == top2); // if stacks meet, array is full
 }
 
-// check stack is full
-int isfull(int top)
+// check if stack 1 is empty
+int isempty1()
 {
-    return (top == MAX_SIZE - 1);
+    return (top1 == -1);
 }
 
-void push(int *stack, int *top, int item)
+// check stack 2 is empty
+int isempty2()
 {
-    if (isfull(*top))
+    return (top2 == MAX_SIZE);
+}
+
+void push1(int item)
+{
+    if (isfull())
     {
         printf("stack is full");
         return;
     }
-    stack[++(*top)] = item;
+    stack[++top1] = item;
 }
 
-int pop(int *stack, int *top)
+void push2(int item)
 {
-    if (isempty(*top))
+    if (isfull())
     {
-        printf("stack is empty\n");
+        printf("stack is full");
+        return;
+    }
+    stack[--top2] = item;
+}
+
+int pop1()
+{
+    if (isempty1())
+    {
+        printf("stack 1 is empty\n");
         return -1;
     }
-    return stack[(*top)--];
+    return stack[top1--];
 }
 
-int main() {
-    int *stack1 = createstack();
-    int top1 = -1;
-    push(stack1, &top1, 50);
-    push(stack1, &top1, 40);
-    push(stack1, &top1, 30);
-    push(stack1, &top1, 10);
+int pop2()
+{
+    if (isempty2)
+    {
+        printf("stack 2 is empty");
+        return -1;
+    }
+    stack[top2++];
+}
 
-    printf("\nElements in Stack-1 are: ");
+void printstack1()
+{
+    printf("elements in stack-1 are: ");
     for (int i = 0; i <= top1; i++)
     {
-        printf("%d ", stack1[i]);
+        printf("%d ", stack[i]);
     }
     printf("\n");
+}
 
-    int *stack2 = createstack();
-    int top2 = -1;
-
-    push(stack2, &top2, 70);
-    push(stack2, &top2, 60);
-    push(stack2, &top2, 50);
-    push(stack2, &top2, 40);
-    push(stack2, &top2, 20);
-
-    printf("Elements in Stack-2 are:");
-    for (int i = 0; i <= top2; i++)
+void printstack2()
+{
+    printf("elements in stack-2 are: ");
+    for (int i = MAX_SIZE - 1; i >= top2; i--)
     {
-        printf("%d ", stack2[i]);
+        printf("%d ", stack[i]);
     }
-    
+    printf("\n");
+}
+int main()
+{
+
+    push1(50);
+    push1(40);
+    push1(30);
+    push1(10);
+    printstack1();
+
+    push2(70);
+    push2(60);
+    push2(50);
+    push2(40);
+    push2(20);
+    printstack2();
+
+    return 0;
 }
