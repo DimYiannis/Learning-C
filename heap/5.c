d596d82c93e9354124b53e91443f847c982463ac
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-03-25 20:34:41 +0200

    Update 5.c

--- heap/5.c
+++ heap/5.c
@@ -5,10 +5,80 @@ Write a C program to implement two stacks in a single array and performs push an
 
 Expected Output:
 
-Elements in Stack-1 are: 50 40 30 10 
-Elements in Stack-2 are: 70 60 50 40 20 
+Elements in Stack-1 are: 50 40 30 10
+Elements in Stack-2 are: 70 60 50 40 20
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_SIZE 100
 
+// create empty stack
+int *createstack()
+{
+    int *stack = (int *)malloc(MAX_SIZE * sizeof(int));
+    return stack;
+}
+// check if stack is empty
+int isempty(int top)
+{
+    return (top == -1);
+}
+
+// check stack is full
+int isfull(int top)
+{
+    return (top == MAX_SIZE - 1);
+}
+
+void push(int *stack, int *top, int item)
+{
+    if (isfull(*top))
+    {
+        printf("stack is full");
+        return;
+    }
+    stack[++(*top)] = item;
+}
+
+int pop(int *stack, int *top)
+{
+    if (isempty(*top))
+    {
+        printf("stack is empty\n");
+        return -1;
+    }
+    return stack[(*top)--];
+}
+
+int main() {
+    int *stack1 = createstack();
+    int top1 = -1;
+    push(stack1, &top1, 50);
+    push(stack1, &top1, 40);
+    push(stack1, &top1, 30);
+    push(stack1, &top1, 10);
+
+    printf("\nElements in Stack-1 are: ");
+    for (int i = 0; i <= top1; i++)
+    {
+        printf("%d ", stack1[i]);
+    }
+    printf("\n");
+
+    int *stack2 = createstack();
+    int top2 = -1;
+
+    push(stack2, &top2, 70);
+    push(stack2, &top2, 60);
+    push(stack2, &top2, 50);
+    push(stack2, &top2, 40);
+    push(stack2, &top2, 20);
+
+    printf("Elements in Stack-2 are:");
+    for (int i = 0; i <= top2; i++)
+    {
+        printf("%d ", stack2[i]);
+    }
+    
+}
