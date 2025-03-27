72233cfab950e717306895e132b032ce927d9dc7
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-03-27 21:08:06 +0200

    Update 7.c

--- heap/7.c
+++ heap/7.c
@@ -30,22 +30,35 @@ int stack[MAX_SIZE];
 int top = -1;
 
 void push(char data) {
-    if (top == MAX_SIZE)
+    if (top == MAX_SIZE - 1)
     {
         printf("stack is overflown\n");
-        return -1;
     }
-    top++;
-    stack[top] = data;
+    char *ptr = data; //pointer to the beginning of the string
+    while (*ptr != '\0'  && *ptr !='\n')
+    {
+        stack[++top] = *ptr;
+        ptr++;
+    }
 }
 
-int balance(int *top) {
+int pop() {
+    if (top == -1)
+    {
+        return 0;
+    }
+    return stack[top--];
+}
 
+int balance(int top) {
+    if (stack[top] == ')' || stack[top] == '}' || stack[top] == ']')
+    return 0;
 }
 
 int main() {
     char data[MAX_SIZE];
     printf("Input an expression in parentheses  : ");
-    scanf("%s", data);
+    fgets(data, MAX_SIZE, stdin);
+    printf("%s", data);
     push(data);
 }
