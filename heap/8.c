25f10b99f77cfafb3c403f172bf59bcb169e6fcc
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-04-01 22:01:14 +0300

    Update 8.c

--- heap/8.c
+++ heap/8.c
@@ -39,3 +39,77 @@ The next larger elements are:
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 20
+
+int stack[MAX_SIZE];
+int top = -1;
+
+void push(char data)
+{
+    if (top == MAX_SIZE - 1)
+    {
+        printf("stack is full\n");
+    }
+    stack[++top] = data;
+}
+
+int pop()
+{
+    if (top == -1)
+    {
+        printf("stack is empty\n");
+        return -1;
+    }
+    return stack[top--];
+}
+
+void next_greater(char *arr)
+{
+    for (int i = 0; arr[i] != '\0' && arr[i] !='\n'; i++)
+    {
+        int found = 0;
+
+        for (int j = 1; arr[j] != '\0' && arr[j] !='\n'; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                push(arr[j]);
+                found = 1;
+                break;
+            }
+        
+        }
+        if (!found)
+        {
+            push(-1);
+        }
+        
+    }
+    for (int i = 0; i<top; i++)
+    {
+        if (stack[i] == -1)
+        {
+            printf("%c --> -1 \n", arr[i]);
+        }
+        else 
+        {
+            printf("%c --> %c \n", arr[i], stack[i]);
+        }
+    }
+    
+}
+
+int main()
+{
+    char arr[MAX_SIZE];
+    printf("insert numbers\n");
+    fgets(arr, MAX_SIZE, stdin);
+    printf("Elements in the array are: ");
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        printf("%c ", arr[i]);
+    }
+    printf("\n");
+    printf("The next larger elements are: \n");
+    next_greater(arr);
+}
